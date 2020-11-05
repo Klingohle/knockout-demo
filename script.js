d7865a9144b012c9dfe56f4dde504c223ffff935
Andreas <andreas@rudischhauser.de>
2020-11-05 14:43:57 +0100

    Initial project setup with extended

---
+++ script.js
@@ -0,0 +1,36 @@
+
+ko.extenders.logChange = function(target, text) {
+    target.subscribe(function(newValue) {
+       console.log(text + ": " + newValue);
+    });
+    return target;
+};
+
+// Here's my data model
+
+var ViewModel = function(first, last) {
+    this.firstName = ko.observable(first);
+    this.lastName = ko.observable(last).extend({logChange: "last name has changed"});;
+ 
+    this.firstName.extend({
+        required:  {
+            message: "Required",
+            onlyIf: function(){
+                return ko.unwrap(this.firstName) == "";
+            }
+        }        
+    });
+
+    this.fullName = ko.pureComputed(function() {
+        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+        return this.firstName() + " " + this.lastName();
+    }, this);
+
+
+};
+ 
+ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+
+
+
+//https://www.npmjs.com/package/knockout-validations-extender
